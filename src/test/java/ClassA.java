0fe3f092287203ec67ed84379e4039e5f558f00d
AravinthraLingam <aravinthralingam@gmail.com>
2025-07-20 19:32:50 +0530

    First push to master

---
+++ src/test/java/ClassA.java
@@ -0,0 +1,16 @@
+
+public class ClassA {
+	private void csk() {
+		// TODO Auto-generated method stub
+
+	}
+	private void mi() {
+		// TODO Auto-generated method stub
+
+	}
+	private void rcb() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
